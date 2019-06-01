7a55574b88c9a60a6c3e591e2df0cb0488abf4ce
mk668a <msftoaa@gmail.com>
2019-06-02 01:48:07 +0900

    [post]places,genres,urlsが未入力でも投稿されるように修正

--- src/components/postItem.vue
+++ src/components/postItem.vue
@@ -67,12 +67,23 @@ export default {
           message: '全ての必須項目に入力してください'
         })
       } else {
+        
+        if (this.places != "") {
+          this.places = this.places.slice(0, -1)
+        }
+        if (this.urls != "") {
+          this.urls = this.urls.slice(0, -1)
+        }
+        if (this.genres != "") {
+          this.genres = this.genres.slice(0, -1)
+        }
+
         var postData = {
           img: this.img,
           name: this.name,
-          places: this.places.slice(0, -1),
-          url: this.urls.slice(0, -1),
-          genre: this.genres.slice(0, -1),
+          places: this.places,
+          url: this.urls,
+          genre: this.genres,
           created_at: Date.now(),
           uid: userId,
         };
@@ -155,217 +166,217 @@ export default {
 </script>
 
 <style lang="scss">
-.postItem>form>.el-switch.is-checked,
-.postItem>form>.el-switch {
-  padding-top: 30px;
+.postItem > form > .el-switch,
+.postItem > form > .el-switch.is-checked {
+    padding-top: 30px;
 }
 
 .postItem {
-  height: 100%;
+    height: 100%;
 }
 
-.postItem>form>div,
-.el-form.demo-dynamic {
-  margin: 30px auto;
+.el-form.demo-dynamic,
+.postItem > form > div {
+    margin: 30px auto;
 }
 
 /*  */
 
-.el-form-item__content>.el-input:nth-of-type(1),
-.el-form-item__content>.el-input:nth-of-type(1)>.el-input__inner {
-  width: 100px;
+.el-form-item__content > .el-input:nth-of-type(1),
+.el-form-item__content > .el-input:nth-of-type(1) > .el-input__inner {
+    width: 100px;
 }
 
-.el-form-item__content>.el-input:nth-of-type(2),
-.el-form-item__content>.el-input:nth-of-type(2)>.el-input__inner {
-  width: 300px;
+.el-form-item__content > .el-input:nth-of-type(2),
+.el-form-item__content > .el-input:nth-of-type(2) > .el-input__inner {
+    width: 300px;
 }
 
 .el-form-item {
-  display: flex;
+    display: flex;
 }
 
 .el-form-item__content:nth-child(1) {
-  width: 100%;
+    width: 100%;
 }
 
 .el-form-item__label {
-  width: 40vw;
-  text-align: right;
+    width: 40vw;
+    text-align: right;
 }
 
 .el-form-item__label {
-  font-size: 16px;
+    font-size: 16px;
 }
 
 /*  */
 
-.postItem>form>.imgupload {
-  background: #d2d2d2;
-  padding: 40px;
+.postItem > form > .imgupload {
+    background: #d2d2d2;
+    padding: 40px;
 }
 
-.postItem>form>img {
-  margin: 30px auto;
-  border-radius: 1em;
-  height: 480px;
-  width: auto;
+.postItem > form > img {
+    margin: 30px auto;
+    border-radius: 1em;
+    height: 480px;
+    width: auto;
 }
 
-.postItem>form>div>a {
-  border: solid 1px #F56C6C;
-  border-radius: 0.5em;
-  background-color: #F56C6C;
-  font-size: 5px;
-  margin: auto 10px;
+.postItem > form > div > a {
+    border: solid 1px #F56C6C;
+    border-radius: 0.5em;
+    background-color: #F56C6C;
+    font-size: 5px;
+    margin: auto 10px;
 }
 
-.postItem>form>.preview {
-  margin: auto 100px;
+.postItem > form > .preview {
+    margin: auto 100px;
 }
 
-.postItem>form>div>button {
-  padding: 20px 50px;
+.postItem > form > div > button {
+    padding: 20px 50px;
 }
 
-.postItem>form>div>.el-input {
-  width: auto;
+.postItem > form > div > .el-input {
+    width: auto;
 }
 
-.postItem>form>div>.el-input>input {
-  width: 300px;
+.postItem > form > div > .el-input > input {
+    width: 300px;
 }
 
-.postItem>form>div>.el-input:nth-child(1)>input {
-  margin-left: 45px;
+.postItem > form > div > .el-input:nth-child(1) > input {
+    margin-left: 45px;
 }
 
-.postItemContainer>form>div>input {
-  height: 30px;
-  border-radius: 1em;
-  border: solid 1px #e6e6e6;
+.postItemContainer > form > div > input {
+    height: 30px;
+    border-radius: 1em;
+    border: solid 1px #e6e6e6;
 }
 
-.postItem>form>.preview>.original {
-  border-radius: 1em;
-  border: solid 1px #e6e6e6;
-  overflow: hidden;
-  padding: 50px;
+.postItem > form > .preview > .original {
+    border-radius: 1em;
+    border: solid 1px #e6e6e6;
+    overflow: hidden;
+    padding: 50px;
 }
 
-.postItem>form>.preview>.original>div {
-  display: block;
+.postItem > form > .preview > .original > div {
+    display: block;
 }
 
-.postItem>form>.preview>.original>img,
-.postItem>img {
-  width: auto;
-  height: 320px;
+.postItem > form > .preview > .original > img,
+.postItem > img {
+    width: auto;
+    height: 320px;
 }
 
-.postItem>form>.preview>.original>.data>h4 {
-  display: block;
-  margin: auto 50px;
+.postItem > form > .preview > .original > .data > h4 {
+    display: block;
+    margin: auto 50px;
 }
 
 .avatar-uploader .el-upload {
-  border: 1px dashed #d9d9d9;
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
 }
 
 .el-upload.el-upload--text {
-  background-color: #e5e5e5;
+    background-color: #e5e5e5;
 }
 
 .avatar-uploader .el-upload:hover {
-  border-color: #409EFF;
+    border-color: #409EFF;
 }
 
 .avatar-uploader-icon {
-  font-size: 28px;
-  color: #d2d2d2;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
-  text-align: center;
+    font-size: 28px;
+    color: #d2d2d2;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
 }
 
 .el-upload.el-upload--text::before {
-  content: '画像選択';
-  white-space: pre;
-  position: absolute;
-  top: 50px;
-  left: 30px;
-  color: #d2d2d2;
-  font-size: 30px;
+    content: '画像選択';
+    white-space: pre;
+    position: absolute;
+    top: 50px;
+    left: 30px;
+    color: #d2d2d2;
+    font-size: 30px;
 }
 
 .el-icon-circle-plus-outline.avatar-uploader-icon::before {
-  position: absolute;
-  top: 20px;
-  left: 75px;
+    position: absolute;
+    top: 20px;
+    left: 75px;
 }
 
 .avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
+    width: 178px;
+    height: 178px;
+    display: block;
 }
 
 .el-select .el-input {
-  width: 110px;
+    width: 110px;
 }
 
 .input-with-select .el-input-group__prepend {
-  background-color: #fff;
+    background-color: #fff;
 }
 
 @media screen and (max-width:768px) {
-  .postItem>form>.preview {
-    margin: auto 20px !important;
-  }
+    .postItem > form > .preview {
+        margin: auto 20px !important;
+    }
 
-  .postItem>form>.preview>.original>div {
-    display: block;
-  }
+    .postItem > form > .preview > .original > div {
+        display: block;
+    }
 
-  .el-form-item>.el-form-item__content {
-    margin: 20px auto !important;
-  }
+    .el-form-item > .el-form-item__content {
+        margin: 20px auto !important;
+    }
 
-  .el-form-item {
-    display: block;
-    margin: auto 37.5px;
-  }
+    .el-form-item {
+        display: block;
+        margin: auto 37.5px;
+    }
 
-  .el-form-item__content {
-    margin: 0 !important;
-  }
+    .el-form-item__content {
+        margin: 0 !important;
+    }
 
-  .el-form-item__label {
-    width: 300px !important;
-    text-align: center;
-  }
+    .el-form-item__label {
+        width: 300px !important;
+        text-align: center;
+    }
 
-  .postItem>form>.preview>.original>img {
-    width: 100%;
-    height: auto;
-  }
+    .postItem > form > .preview > .original > img {
+        width: 100%;
+        height: auto;
+    }
 
-  .postItem>form>img {
-    height: 240px;
-    width: auto;
-  }
+    .postItem > form > img {
+        height: 240px;
+        width: auto;
+    }
 
-  /* .postItem>form>.preview>.original>div>.data>h1::before{
+    /* .postItem>form>.preview>.original>div>.data>h1::before{
     height: 0;
   } */
-  .postItem>form>.preview>.original>.data>h4 {
-    margin: 0 !important;
-    font-size: 10px;
-  }
+    .postItem > form > .preview > .original > .data > h4 {
+        margin: 0 !important;
+        font-size: 10px;
+    }
 }
 </style>
